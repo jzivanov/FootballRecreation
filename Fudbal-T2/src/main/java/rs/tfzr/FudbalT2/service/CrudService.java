01a927e3a07f0cf418f90e81c8c2d7754872708d
Miroslav <elemmiki@gmail.com>
2014-08-18 21:28:29 +0200

    Added CrudService.java

---
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/CrudService.java
@@ -0,0 +1,46 @@
+package rs.tfzr.FudbalT2.service;
+
+import java.util.List;
+
+import rs.tfzr.FudbalT2.model.AbstractBaseEntity;
+
+/**
+ * CRUD (Create/Read/Update/Delete) service.
+ * 
+ * @author d.gajic
+ * 
+ * @param <T>
+ */
+public interface CrudService<T extends AbstractBaseEntity> {
+
+	/**
+	 * Find and return entity with passed id.
+	 * 
+	 * @param id of the entity to return
+	 * @return entity with passed id or null if not found
+	 */
+	T findOne(Long id);
+
+	/**
+	 * Return back all existing entities.
+	 * 
+	 * @return list of existing entities, empty list if there are no entities
+	 */
+	List<T> findAll();
+
+	/**
+	 * Save entity and return saved instance (with id set).
+	 * 
+	 * @param entity to be saved
+	 * @return saved instance
+	 */
+	T save(T t);
+
+	/**
+	 * Remove entity with passed id.
+	 * 
+	 * @param id of the entity to be removed
+	 * @throws IllegalArgumentException if there is no entity with passed id
+	 */
+	void remove(Long id) throws IllegalArgumentException;
+}
