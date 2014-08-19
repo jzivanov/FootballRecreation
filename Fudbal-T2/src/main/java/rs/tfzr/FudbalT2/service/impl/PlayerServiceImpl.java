db908a4171b6649ecc685ac51ef3c941a73e2ee4
jovan <zjovan.ost@gmail.com>
2014-08-19 15:54:24 +0200

    Implemented Player Service

---
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,41 @@
+package rs.tfzr.FudbalT2.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import rs.tfzr.FudbalT2.model.Player;
+import rs.tfzr.FudbalT2.repository.PlayerRepository;
+import rs.tfzr.FudbalT2.service.PlayerService;
+
+public class PlayerServiceImpl implements PlayerService
+{
+	@Autowired
+	PlayerRepository playerRepository;
+	
+	@Override
+	public Player findOne(Long id) {
+		return playerRepository.findOne(id);
+	}
+
+	@Override
+	public List<Player> findAll() {
+		return playerRepository.findAll();
+	}
+
+	@Override
+	public Player save(Player player) {
+		return playerRepository.save(player);
+	}
+
+	@Override
+	public void remove(Long id) throws IllegalArgumentException {
+		Player player = playerRepository.findOne(id);
+		if(player == null)
+		{
+            throw new IllegalArgumentException("Player with id " + id + " does not exist.");
+		}
+		playerRepository.delete(id);
+	}
+
+}
