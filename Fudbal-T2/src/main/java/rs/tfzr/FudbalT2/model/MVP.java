ab3b69760502fa6dbb594bc2b0078a959ed4a7c2
jovan <zjovan.ost@gmail.com>
2014-08-19 10:11:34 +0200

    Added MVP, changed Player model

---
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/model/MVP.java
@@ -0,0 +1,11 @@
+package rs.tfzr.FudbalT2.model;
+
+public class MVP extends AbstractBaseEntity
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/model/Player.java
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/model/Player.java
@@ -2,10 +2,70 @@ package rs.tfzr.FudbalT2.model;
 
 public class Player extends AbstractBaseEntity
 {
-
 	/**
-	 * 
+	 * serialVersionUID
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public enum Team
+	{
+		Home,
+		Away
+	}
+	
+	public Player()
+	{
+		
+	}
+	
+	public Player(User user)
+	{
+		this.user = user;
+	}
+	
+	private Team team;
+	private User user;
+	private Exhibition exhibition;
 
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Exhibition getExhibition() {
+		return exhibition;
+	}
+
+	public void setExhibition(Exhibition exhibition) {
+		this.exhibition = exhibition;
+	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + user.hashCode();
+        result = 31 * result + exhibition.hashCode();
+        result = 31 * result + team.hashCode();
+        return result;
+    }
 }
