e991694736301bd9136020dec12668b2f66d9434
Nenad Mirkovic <nneennaadd@gmail.com>
2014-08-19 11:03:37 +0200

    Updated User.java

--- Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/model/User.java
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/model/User.java
@@ -1,10 +1,66 @@
 package rs.tfzr.FudbalT2.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class User extends AbstractBaseEntity {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3089317421338985268L;
 
-	
-	
-	
-	
+	private String firstName;
+	private String lastName;
+	@NotEmpty
+	@Size(min = 4, max = 20)
+	private String password;
+	@NotEmpty
+	private String confirmPassword;
+	@NotEmpty
+	@Email
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
