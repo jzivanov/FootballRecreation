3d61939696bd31daae1916bfa216168fd550ecee
Miroslav <elemmiki@gmail.com>
2014-08-20 15:01:22 +0200

    Update to the last commit

--- Fudbal-T2/src/test/java/rs/tfzr/FudbalT2/service/memory/InMemoryExhibitionServiceTest.java
+++ Fudbal-T2/src/test/java/rs/tfzr/FudbalT2/service/memory/InMemoryExhibitionServiceTest.java
@@ -104,7 +104,7 @@ public class InMemoryExhibitionServiceTest {
 		Exhibition exhibition = inMemoryExhibitionService.findOne(1L);
 
 		inMemoryExhibitionService.addPlayer(exhibition, 1L);
-		inMemoryPlayerService.findOne(1L).setTeam(Player.Team.Home);
+		inMemoryExhibitionService.addToTeam(inMemoryPlayerService.findOne(1L).getId(), Player.Team.Home);
 		Assert.assertNotNull(inMemoryPlayerService.findOne(1L).getTeam());
 		Assert.assertTrue(inMemoryPlayerService.findOne(1L).getTeam() == Player.Team.Home);
 
