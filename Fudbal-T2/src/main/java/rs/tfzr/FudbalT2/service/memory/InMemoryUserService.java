94f257e12f9c7eaeca6626b7ae2055d684abba3a
jovan <zjovan.ost@gmail.com>
2014-08-20 13:11:40 +0200

    [Created: ] InMemoryUserService.java

---
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/memory/InMemoryUserService.java
@@ -0,0 +1,9 @@
+package rs.tfzr.FudbalT2.service.memory;
+
+import rs.tfzr.FudbalT2.model.User;
+import rs.tfzr.FudbalT2.service.UserService;
+
+public class InMemoryUserService extends AbstractInMemoryService<User> implements UserService
+{
+
+}
