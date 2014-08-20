14203748e586ed89a698d174c698cae54dd6d04e
Miroslav <elemmiki@gmail.com>
2014-08-20 13:20:11 +0200

    Updated InMemoryExhibitionServiceTest.java

--- Fudbal-T2/src/test/java/rs/tfzr/FudbalT2/service/memory/InMemoryExhibitionServiceTest.java
+++ Fudbal-T2/src/test/java/rs/tfzr/FudbalT2/service/memory/InMemoryExhibitionServiceTest.java
@@ -5,10 +5,15 @@ package rs.tfzr.FudbalT2.service.memory;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import rs.tfzr.FudbalT2.model.Exhibition;
+import rs.tfzr.FudbalT2.model.Player;
+import rs.tfzr.FudbalT2.model.User;
 
 /**
  * @author Miroslav
@@ -16,15 +21,34 @@ import rs.tfzr.FudbalT2.model.Exhibition;
  */
 public class InMemoryExhibitionServiceTest {
 
-	private InMemoryExhibitionService inMemoryService;
+	private InMemoryExhibitionService inMemoryExhibitionService;
+	private InMemoryUserService inMemoryUserService;
+	
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		inMemoryService = new InMemoryExhibitionService();
+		inMemoryExhibitionService = new InMemoryExhibitionService();
 		Exhibition exhibition = new Exhibition();
+		User user = new User();
+		Player player = new Player();
+
+		exhibition.setId(1L);
+		exhibition.setExhibitionStart(new Date());
+
+		user.setId(1L);
+		user.setEmail("zjovan.ost@gmail.com");
+		user.setFirstName("jovan");
+		user.setLastName("zivanov");
+
+		player.setId(1L);
+		player.setUser(user);
+		player.setExhibition(exhibition);
+		
+		inMemoryExhibitionService.save(exhibition);
+		inMemoryUserService.save(user);
 	}
 
 	/**
@@ -34,7 +58,16 @@ public class InMemoryExhibitionServiceTest {
 	 */
 	@Test
 	public void testAddPlayer() {
-		fail("Not yet implemented");
+		Exhibition exhibition = inMemoryExhibitionService.findOne(1L);
+		
+//		Assert.assertNotNull(exhibition.getPlayers());
+//		Assert.assertTrue(exhibition.getPlayers().size() == 0);
+
+		inMemoryExhibitionService.addPlayer(exhibition, 1L);
+
+		Assert.assertNotNull(exhibition.getPlayers());
+		Assert.assertTrue(exhibition.getPlayers().size() == 1);
+		Assert.assertNotNull(exhibition.getPlayers().get(0));
 	}
 
 	/**
