cb5afc396fa1255ad19ddb61d1150d4c8833575d
jovan <zjovan.ost@gmail.com>
2014-08-22 09:21:53 +0200

    [Updated: ] InMemoryUserService

--- Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/memory/InMemoryUserService.java
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/memory/InMemoryUserService.java
@@ -1,8 +1,11 @@
 package rs.tfzr.FudbalT2.service.memory;
 
+import org.springframework.stereotype.Service;
+
 import rs.tfzr.FudbalT2.model.User;
 import rs.tfzr.FudbalT2.service.UserService;
 
+@Service
 public class InMemoryUserService extends AbstractInMemoryService<User>
 		implements UserService {
 
