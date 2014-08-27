51620d2e737757641774573925a963587e8f5990
Nenad Mirkovic <nneennaadd@gmail.com>
2014-08-27 14:40:55 +0200

    Updated UserController.java Added UserServiceImpl.java

---
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package rs.tfzr.FudbalT2.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import rs.tfzr.FudbalT2.model.User;
+import rs.tfzr.FudbalT2.repository.UserRepository;
+import rs.tfzr.FudbalT2.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User findOne(Long id) {
+		return userRepository.findOne(id);
+
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userRepository.findAll();
+
+	}
+
+	@Override
+	public void remove(Long id) throws IllegalArgumentException {
+		userRepository.delete(id);
+
+	}
+
+}
--- Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/web/controller/UserController.java
+++ Fudbal-T2/src/main/java/rs/tfzr/FudbalT2/web/controller/UserController.java
@@ -2,15 +2,24 @@ package rs.tfzr.FudbalT2.web.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import rs.tfzr.FudbalT2.model.User;
+import rs.tfzr.FudbalT2.service.UserService;
 
 @Controller
 public class UserController {
+
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String viewRegistration(Map<String, Object> model) {
@@ -20,17 +29,23 @@ public class UserController {
 		return "register";
 	}
 
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String processRegistration(@ModelAttribute("userFormRegister") User user,
-			Map<String, Object> model) {
+	@RequestMapping(params = "register", method = RequestMethod.POST)
+	public String processRegistration(@Valid User user,
+			BindingResult bindingResult, Model model) {
 
-        System.out.println("RegisterSuccess");
+		String viewUser;
+		if (!bindingResult.hasErrors()) {
+			userService.save(user);
+			viewUser = "redirect:registerSuccess";
+		} else {
+			model.addAttribute("user", user);
+			viewUser = "register";
+		}
 
-      //Register logic here
-		return "registerSuccess";
+		return viewUser;
+
 	}
-	
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String viewLogin(Map<String, Object> model) {
 		User userFormLogin = new User();
@@ -42,9 +57,8 @@ public class UserController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String processLogin(@ModelAttribute("userFormLogin") User user,
 			Map<String, Object> model) {
-		//Login logic here
-        System.out.println("LoginSuccess");
-
+		// Login logic here
+		System.out.println("LoginSuccess");
 
 		return "loginSuccess";
 	}
